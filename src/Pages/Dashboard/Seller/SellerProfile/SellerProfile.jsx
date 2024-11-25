c8c1164f6e48f35082eb89ed4b19bde8b358cf12
mohammad syed hasan <syedhasanmohammad@gmail.com>
2024-11-25 22:56:23 +0600

    added  seller profile

--- src/Pages/Dashboard/Seller/SellerProfile/SellerProfile.jsx
+++ src/Pages/Dashboard/Seller/SellerProfile/SellerProfile.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
 import { AuthContext } from "../../../../Provider/AuthProvider";
-import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
 
 const SellerProfile = () => {
   const axiosSecure = useAxiosSecure();
@@ -18,10 +18,37 @@ const SellerProfile = () => {
     };
     fetchSalesRevenue();
   }, [axiosSecure, user?.email]);
+  // Filter data for paid and pending statuses
+  const paidData = salesData?.find((item) => item.status === "paid");
+  const pendingData = salesData?.find((item) => item.status === "pending");
 
   console.log("sales Data", salesData);
 
-  return <div></div>;
+  return (
+    <div className="bg-[#e2e2e2] max-w-7xl mx-auto h-screen flex items-center justify-center">
+      <Helmet>
+        <title>HealthBox | Seller Profile</title>
+      </Helmet>
+      <div className="container mx-auto">
+        <div className=" grow flex-1 grid grid-cols-1 lg:grid-cols-2 justify-center items-center  px-7 gap-5">
+          {/* card2 */}
+          <div className="card bg-PrimaryColor    max-w-5xl  p-7 border border-SecondaryColor bg-gradient-to-tr from-SecondaryColor to-PrimaryColor text-white">
+            <div className="card-body">
+              <h2 className="card-title">Paid Amount</h2>
+              <p>${paidData?.amount.toFixed(2)}</p>
+            </div>
+          </div>
+          {/* card3 */}
+          <div className="card bg-PrimaryColor   max-w-5xl  p-7 border border-SecondaryColor bg-gradient-to-tr from-SecondaryColor to-PrimaryColor text-white">
+            <div className="card-body">
+              <h2 className="card-title">Pending Amount</h2>
+              <p>${pendingData?.amount.toFixed(2)}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SellerProfile;
